0efbf55117986787404660aed9fd53f835b572f2
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-02 16:41:27 +0200

    change main to actual main

--- js-functions_functions-with-parameters/js/index.js
+++ js-functions_functions-with-parameters/js/index.js
@@ -1,20 +1,20 @@
-console.clear();
+onsole.clear();
 
-logSeparator(1);
+console.log("-------------");
+console.log("Exercise 1:");
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-
-logText();
-// Paranthesis were missing
+// logText;
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-logSeparator(2);
+console.log("-------------");
+console.log("Exercise 2:");
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
 */
@@ -37,17 +37,12 @@ greetMary();
 
 // --v-- write your code here --v--
 
-function greet(name) {
-  console.log("Welcome " + name + ", good to see you again!");
-}
-
-greet("Alice");
-greet("Bob");
-greet("Mary");
+function greet() {}
 
 // --^-- write your code here --^--
 
-logSeparator(3);
+console.log("-------------");
+console.log("Exercise 3:");
 /*
 3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as a parameter and logs the respective separator. Then use this function to replace the existing separator logs in this javascript file.
 
@@ -56,8 +51,4 @@ Note: Separation text means the text that is logged into the console before each
 
 // --v-- write your code here --v--
 
-function logSeparator(excerciseNumber) {
-  console.log("-------------");
-  console.log("Exercise: " + excerciseNumber);
-}
 // --^-- write your code here --^--
