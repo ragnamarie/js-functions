8c699175be75b1fe5f2b6a553e2e34e069e9f98a
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-03 12:18:59 +0200

    pizzagain

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -19,6 +19,25 @@ pizzaInput2.addEventListener("input", () => {
 // Task 1
 // define the function calculatePizzaGain here
 
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = (diameter1 / 2) ** 2 * Math.PI;
+  const area2 = (diameter2 / 2) ** 2 * Math.PI;
+  const pizzaGain = ((area2 - area1) / area1) * 100;
+  output.textContent = Math.round(pizzaGain);
+}
+
+// First, we want to calculate the difference of the two pizza areas for two given pizza diameters. For that, do the following:
+
+// 1. Write a function `calculatePizzaGain` which expects the two diameters of the two pizzas: `diameter1` and `diameter2`. (Not the radii of the pizzas!)
+// 2. Inside this function, calculate the areas of both pizzas.
+//    > 💡 you can find the formula for that online. The value of pi can be accessed via `Math.PI`.
+// 3. Calculate the pizza gain in percent relative to the first pizza
+//    > 💡 The formula is: `(area2 - area1) / area1 * 100`.
+// 4. Set the `textContent` of the output element to that value. Use `Math.round(calculatedValue)` to round the pizza gain to the next whole integer value.
+// 5. Inside both event listeners, save the current values of both pizza inputs in two variables `pizzaSize1` and `pizzaSize2`
+//    > 💡 You can use the queried elements and get their value like this: `pizzaInput.value`
+// 6. Now call the function and pass in the two pizza sizes as the arguments.
+
 // Task 2
 // define the function updatePizzaDisplay here
 
