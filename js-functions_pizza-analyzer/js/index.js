1d9e45d010814e70fe1ebf374d3a199b4b434c88
ragnamarie <ragna.steinboemer@gmail.com>
2023-10-04 12:10:33 +0200

    add progress bar

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,17 +9,19 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  let pizzaSize1 = pizzaInput1.value;
-  let pizzaSize2 = pizzaInput2.value;
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  let pizzaSize1 = pizzaInput1.value;
-  let pizzaSize2 = pizzaInput2.value;
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
@@ -42,3 +44,15 @@ function updatePizzaDisplay(pizzaElement, newSize) {
 
 // Task 3
 // define the function updateOutputColor here
+
+function updateOutputColor(size1, size2) {
+  const area1 = (size1 / 2) ** 2 * Math.PI;
+  const area2 = (size2 / 2) ** 2 * Math.PI;
+  const pizzaGain = ((area2 - area1) / area1) * 100;
+
+  if (pizzaGain >= 0) {
+    outputSection.style.backgroundColor = "var(--green)";
+  } else {
+    outputSection.style.backgroundColor = "var(--red)";
+  }
+}
